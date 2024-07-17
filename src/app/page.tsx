ffdf24330ce0f27944fa7a8d36379b8b7cd71fc3
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-07-17 18:53:43 +0100

    add date ranges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,16 +16,19 @@ const formatDate = (date: Date): string => {
 }
 
 export default function App() {
-  // Get yesterdays date //
-const getYesterdayDate = (): Date => {
+  // Get dates to be used in db call //
+const getDateRange = (): Date[] => {
   const yesterday = new Date();
   yesterday.setDate(yesterday.getDate() - 1);
 
-  return yesterday;
+  const sevenDaysAgo = new Date();
+  sevenDaysAgo.setDate(yesterday.getDate() - 7);
+
+  return [sevenDaysAgo, yesterday];
 }
 
 // Set date as state, default to yesterday //
-const [date, setDate] = useState<Nullable<Date>>(getYesterdayDate);
+const [dateRange, setDateRange] = useState<Nullable<Date[]>>(getDateRange);
 // Main state variables for stat cards //
 const [totalRequests, setTotalRequests] = useState<number>(0);
 const [totalErrors, setTotalErrors] = useState<number>(0);
@@ -34,21 +37,25 @@ const [avgResTime, setAvgResTime] = useState<number>(0);
 
 // Anytime date is updated, call api to call db //
 useEffect(() => {
-  getData(formatDate(date as Date));
-}, [date])
+  if (dateRange) {
+    const formattedDateRange = dateRange.map(date => formatDate(date));
+    getData(formattedDateRange)
+  }
+}, [dateRange])
 
 // DB call with whatever the date is as parameter //
-async function getData(date: string) {
+async function getData(dateRange: string[]) {
   try {
     const response = await fetch('/api/data', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ date })
+      body: JSON.stringify({ dateRange })
     });
 
     const data = await response.json();
+    console.log("Data =", data);
 
     // Call count logs function to populate dashboard with data //
     countLogs(data)
@@ -85,7 +92,7 @@ function countLogs(data: any) {
           <p>0</p>
         </Card>
         <Card className="primaryStat">
-          <Calendar value={date} onChange={(e) => setDate(e.value)} showIcon dateFormat="yy/mm/dd"/>
+          <Calendar value={dateRange ? dateRange[0] : null} onChange={(e) => setDateRange([e.value as Date, dateRange ? dateRange[1] : new Date()])} showIcon dateFormat="yy/mm/dd"/>
         </Card>
       </Card>
 
--- src/pages/api/data.ts
+++ src/pages/api/data.ts
@@ -7,10 +7,12 @@ export default async function data(req: NextApiRequest, res: NextApiResponse) {
         return res.status(405).json({ error: 'Method Not Allowed' })
     }
 
-    const { date } = req.body;
+    const { dateRange } = req.body;
+
+    console.log("Date Range in DB Call is:", dateRange)
 
     try {
-        const data = await db.query(`SELECT * FROM api_usage_aggregate WHERE date = $1`, [date]);
+        const data = await db.query(`SELECT * FROM api_usage_aggregate WHERE date BETWEEN $1 AND $2`, [dateRange[0], dateRange[1]]);
         res.status(200).json(data.rows);
     } catch (error) {
         console.error('Error fetching data:', error);
